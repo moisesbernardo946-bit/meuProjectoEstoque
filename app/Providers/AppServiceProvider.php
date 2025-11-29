36bfc3ee8b8ce0d0cdf0be1bb651ef377f6d8d43
moisesbernardo <moisesbernardo946@gmail.com>
2025-11-29 11:39:18 +0100

    app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\URL;
+
 use App\Models\Client;
 use App\Models\Company;
 
@@ -17,9 +19,13 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Mapeia as strings em entity_type para as classes corretas
         Relation::morphMap([
-            'client'  => Client::class,
+            'client' => Client::class,
             'cliente' => Client::class, // se no teu BD tens 'cliente' (pt), mapeia também
             'company' => Company::class,
             'empresa' => Company::class, // opcional: se houver 'empresa' no BD
