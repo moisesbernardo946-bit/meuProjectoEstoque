400268dec29d1649829b0ecf4833f33a55775ce1
moisesbernardo <moisesbernardo946@gmail.com>
2025-12-01 08:11:04 +0100

    Criar storage link automaticamente

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+         // Força HTTPS em produção
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
+        }
+
+        // Criar storage link automaticamente
+        if (!is_link(public_path('storage'))) {
+            @symlink(storage_path('app/public'), public_path('storage'));
         }
 
         // Mapeia as strings em entity_type para as classes corretas
